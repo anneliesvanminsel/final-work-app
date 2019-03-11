0ed85bd2653ffcbfe9ab8ee3b442a1641133c2d3
Annelies Van Minsel <annelies.van.minsel@student.ehb.be>
2019-03-11 11:32:51 +0100

    Header student - collapse + sass

--- src/app/header/header-student/header-student.component.ts
+++ src/app/header/header-student/header-student.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-student.component.scss']
 })
 export class HeaderStudentComponent implements OnInit {
+  show:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /*
+  * komt van
+  * https://angularfirebase.com/lessons/bootstrap-4-collapsable-navbar-work-with-angular/
+  */
+  toggleCollapse() {
+    this.show = !this.show;
+  }
 }
