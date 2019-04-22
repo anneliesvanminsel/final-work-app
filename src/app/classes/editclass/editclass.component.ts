587f01035421a28bc77b815c09e9dd574e705263
Annelies <anneliesvanminsel@outlook.com>
2019-04-22 11:55:12 +0200

    Added edit class component

---
+++ src/app/classes/editclass/editclass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editclass',
+  templateUrl: './editclass.component.html',
+  styleUrls: ['./editclass.component.scss']
+})
+export class EditclassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
