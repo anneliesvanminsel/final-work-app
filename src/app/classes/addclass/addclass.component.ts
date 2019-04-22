84d40fb15681edc326b7f9abe906221b475f7a27
Annelies <anneliesvanminsel@outlook.com>
2019-04-22 11:54:59 +0200

    Added add class component

---
+++ src/app/classes/addclass/addclass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addclass',
+  templateUrl: './addclass.component.html',
+  styleUrls: ['./addclass.component.scss']
+})
+export class AddclassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
