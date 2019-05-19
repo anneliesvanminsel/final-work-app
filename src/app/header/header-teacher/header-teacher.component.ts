704b48f87c16d7c32143f3272c8ec4200f005a9b
Annelies <anneliesvanminsel@outlook.com>
2019-05-19 18:27:56 +0200

    auth service

--- src/app/header/header-teacher/header-teacher.component.ts
+++ src/app/header/header-teacher/header-teacher.component.ts
@@ -6,19 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-teacher.component.scss']
 })
 export class HeaderTeacherComponent implements OnInit {
-  show:boolean = false;
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  /*
-  * komt van
-  * https://angularfirebase.com/lessons/bootstrap-4-collapsable-navbar-work-with-angular/
-  */
-  toggleCollapse() {
-    this.show = !this.show;
-  }
-
 }
