5ac1776b23b233aadf036485549c83ce95c59e04
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-08 18:06:11 +0200

    Changed the login-form

--- src/app/forms/login/login.component.ts
+++ src/app/forms/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthService } from  '../../services/authservice.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,16 @@ export class LoginComponent implements OnInit {
   @ViewChild('mailInput') mailInputRef: ElementRef;
   @ViewChild('passwordInput') passwordInputRef: ElementRef;
 
-  constructor(private  authService:  AuthService) { }
+  constructor(private  authService:  AuthService, private router: Router) { }
 
   ngOnInit() {}
 
   onLogin() {
     this.authService.login(this.mailInputRef.nativeElement.value, this.passwordInputRef.nativeElement.value);
+    this.authService.isLoggedIn.subscribe((isLoggedIn) => {
+      console.log('account login', this.authService.account);
+      isLoggedIn &&  this.router.navigate(['/index']);
+    });
   }
 
 }
