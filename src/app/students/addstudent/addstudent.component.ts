1bc6631551d79fc9d23c5b5165de014e0bfe98f5
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-10 23:25:33 +0200

    addStudent - fixed

--- src/app/students/addstudent/addstudent.component.ts
+++ src/app/students/addstudent/addstudent.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ClassgroupService} from '../../services/classgroup.service';
+import {StudentTeacherService} from '../../services/student-teacher.service';
+import {Classgroup} from '../../models/classgroup.model';
+import {Student} from '../../models/student.model';
 
 @Component({
   selector: 'app-addstudent',
@@ -6,10 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addstudent.component.scss']
 })
 export class AddstudentComponent implements OnInit {
+  private id: string;
+  private link: string = "/teacher/classes/detail/" ;
+  private class: Classgroup;
 
-  constructor() { }
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('mailInput') mailInputRef: ElementRef;
+
+  newStudent: Student ={
+    name: '',
+    email: '',
+    class_id: '',
+  };
+
+  constructor(
+      private classgroupService: ClassgroupService,
+      private studentTeacherService: StudentTeacherService,
+      private route: ActivatedRoute,
+      private router: Router
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get("classid")
+    });
+    this.link += this.id;
+    this.setClass();
   }
 
+  async setClass() {
+    await this.classgroupService.getClassgroupFromDb(this.id);
+    this.class = this.classgroupService.class;
+  }
+
+  onAddStudent() {
+    this.newStudent.name = this.nameInputRef.nativeElement.value;
+    this.newStudent.email = this.mailInputRef.nativeElement.value;
+    this.newStudent.class_id = this.id;
+    this.studentTeacherService.addStudent(this.newStudent);
+    this.router.navigate([this.link]);
+  }
 }
