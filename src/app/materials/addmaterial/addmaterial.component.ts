79f95f8f5b4f3d4363ec5c7268e0386627b10f6a
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-13 13:20:13 +0200

    added add material component

---
+++ src/app/materials/addmaterial/addmaterial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addmaterial',
+  templateUrl: './addmaterial.component.html',
+  styleUrls: ['./addmaterial.component.scss']
+})
+export class AddmaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
