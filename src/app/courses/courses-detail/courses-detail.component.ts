efae8df076592af1fe655de5c1799582f349c203
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-13 11:52:40 +0200

    Fixing course-detail

--- src/app/courses/courses-detail/courses-detail.component.ts
+++ src/app/courses/courses-detail/courses-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Course} from '../../models/course.model';
+import {CourseService} from '../../services/course.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-courses-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-detail.component.scss']
 })
 export class CoursesDetailComponent implements OnInit {
+  private id: string;
+  private link: string ="/teacher/courses/addmaterial/";
+  private course: Course;
 
-  constructor() { }
+  constructor(private courseService: CourseService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get("courseid")
+    });
+
+    console.log(this.id);
+
+    this.link += this.id;
+
+    this.setCourse();
+  }
+
+  async setCourse() {
+    console.log(this.id);
+    await this.courseService.getCourseFromDb(this.id);
+    this.course = this.courseService.course;
   }
 
 }
