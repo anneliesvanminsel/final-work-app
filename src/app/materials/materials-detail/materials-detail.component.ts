3a521c2764b6adc2b74e7e737235a5d779684d66
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-14 11:41:28 +0200

    Added heading with material-detail

--- src/app/materials/materials-detail/materials-detail.component.ts
+++ src/app/materials/materials-detail/materials-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Material } from '../../models/material.model';
+import {ActivatedRoute} from '@angular/router';
+import {MaterialService} from '../../services/material.service';
+import {CourseService} from '../../services/course.service';
+import {Course} from '../../models/course.model';
 
 @Component({
   selector: 'app-materials-detail',
@@ -8,10 +12,29 @@ import { Material } from '../../models/material.model';
 })
 
 export class MaterialsDetailComponent implements OnInit {
+  private _matid: string;
+  private _courseid: string;
+  private _material: Material;
+  private _course: Course;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private materialService: MaterialService, private courseService: CourseService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this._matid = params.get("materialid");
+      this._courseid = params.get("courseid");
+    });
+    this.setCourse();
+    this.setMaterial();
   }
 
+  async setCourse() {
+    await this.courseService.getCourseFromDb(this._courseid);
+    this._course = this.courseService.course;
+  }
+
+  async setMaterial() {
+    await this.materialService.getMaterialFromDb(this._matid);
+    this._material = this.materialService.material;
+  }
 }
