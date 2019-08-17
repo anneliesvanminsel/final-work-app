395129fa8d1ed0265d02b5a9dc0aa857cda1c3a5
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-17 12:02:33 +0200

    new wordlist is create from database, nu nog kruiswoordraadsel proberen weer te geven

--- src/app/exercise/type/crossword/crossword.component.ts
+++ src/app/exercise/type/crossword/crossword.component.ts
@@ -2,7 +2,10 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {QuestionService} from '../../../services/question.service';
 import {AnswerService} from '../../../services/answer.service';
-
+/*
+    The code written in this component is based on the JQuery-code of 'HoldOffHunger' as seen on Github.
+    link: https://github.com/HoldOffHunger/jquery-crossword-puzzle-generator/blob/master/javascript/crossword-puzzle.js
+ */
 @Component({
     selector: 'app-crossword',
     templateUrl: './crossword.component.html',
@@ -10,34 +13,76 @@ import {AnswerService} from '../../../services/answer.service';
 export class CrosswordComponent implements OnInit {
     @Input() exercise_id: string;
     private questions$: Observable<any>;
-    private answers$: Observable<any>;
-    private _wordList: [];
+    private _wordList = [];
 
-    constructor(private _questionService: QuestionService, private _answerService: AnswerService) { }
+    private _teacherMode: boolean = true; //Show answers in grid
+    private _randomizeWords: boolean = true; //randomize the puzzlewords
+    private _randomizePuzzlePieace: boolean = true; //randomize the 'spine' words'
+    private _randomizeAxis: boolean = true; //randomize the axis of the placement words (down or across)
+    private _randomizeAxisList: boolean = true; //randomize the clue lists
+
+
+
+    constructor(private questionService: QuestionService, private answerService: AnswerService) { }
 
     ngOnInit() {
-        this.questions$ = this._questionService.getQuestionByExercise(this.exercise_id);
-        this.generateWordList();
+        this.questions$ = this.questionService.getQuestionByExercise(this.exercise_id);
+        this.makeCrossword();
     }
 
+    async makeCrossword(){
+        await this.generateWordList();
+        this.createCrosswordPuzzle(this._wordList);
+    }
+
+    //We are creating the list of words with the question and answers that are stored in the database
     generateWordList() {
         this.questions$.subscribe( value => {
                 for(let i=0; i < value.length; i++ ) {
-                    let newWordClue: string[] = [];
-                    console.log(value[i]);
-                    this.answers$ = this._answerService.getAnswerByQuestion(value[i].id);
+                    let newWordClue = [];
 
-                    this.answers$.subscribe( answerList => {
-                        for(let j=0; j < answerList.length; j++ ) {
-                            console.log(answerList[j]);
-                            newWordClue.push(answerList[j].label);
-                        }
+                    this.answerService.getAnswerByQuestion(value[i].id).subscribe( answerList => {
+                        newWordClue.push(answerList[0].label);
+                        newWordClue.push(value[i].title);
+                        this._wordList.push(newWordClue);
                     });
-                    newWordClue.push(value[i].title);
-                    console.log(newWordClue);
                 }
             }
         );
+    }
+
+    //Displaying the crossword
+
+    createCrosswordPuzzle(puzzlewords){
+        console.log(puzzlewords);
+        let wordcount = puzzlewords.length;
+
+        if(!puzzlewords || !wordcount) {
+            console.log("Developer Error : Did you forget to load words?");
+            return false;
+        }
+
+        if(this._randomizeWords) {
+            puzzlewords = this.shuffle(puzzlewords);
+            console.log(puzzlewords);
+        }
+
+    }
+
+    //shuffle the words
+    shuffle(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
     }
 
 }
