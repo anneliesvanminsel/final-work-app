be26a7146eef875937e81c293d27f8afca032d10
anneliesvanminsel <annelies.van.minsel@student.ehb.be>
2019-08-17 11:15:08 +0200

    getting a wordlist out of database

--- src/app/exercise/type/crossword/crossword.component.ts
+++ src/app/exercise/type/crossword/crossword.component.ts
@@ -1,33 +1,43 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {QuestionService} from '../../../services/question.service';
+import {AnswerService} from '../../../services/answer.service';
 
 @Component({
-  selector: 'app-crossword',
-  templateUrl: './crossword.component.html',
-  styleUrls: ['./crossword.component.css']
+    selector: 'app-crossword',
+    templateUrl: './crossword.component.html',
 })
 export class CrosswordComponent implements OnInit {
-  @Input() exercise_id: string;
-  private questions$: Observable<any>;
-  private _wordList: [];
+    @Input() exercise_id: string;
+    private questions$: Observable<any>;
+    private answers$: Observable<any>;
+    private _wordList: [];
 
-  constructor(private questionService: QuestionService) { }
+    constructor(private _questionService: QuestionService, private _answerService: AnswerService) { }
 
-  ngOnInit() {
-    this.questions$ = this.questionService.getQuestionByExercise(this.exercise_id);
-    this.generateWordList();
-  }
-
-  generateWordList() {
-    this.questions$.subscribe( value => {
-          for(let i=0; i < value.length; i++ ) {
-              console.log(value);
-          }
+    ngOnInit() {
+        this.questions$ = this._questionService.getQuestionByExercise(this.exercise_id);
+        this.generateWordList();
     }
 
-    );
+    generateWordList() {
+        this.questions$.subscribe( value => {
+                for(let i=0; i < value.length; i++ ) {
+                    let newWordClue: string[] = [];
+                    console.log(value[i]);
+                    this.answers$ = this._answerService.getAnswerByQuestion(value[i].id);
 
-  }
+                    this.answers$.subscribe( answerList => {
+                        for(let j=0; j < answerList.length; j++ ) {
+                            console.log(answerList[j]);
+                            newWordClue.push(answerList[j].label);
+                        }
+                    });
+                    newWordClue.push(value[i].title);
+                    console.log(newWordClue);
+                }
+            }
+        );
+    }
 
 }
